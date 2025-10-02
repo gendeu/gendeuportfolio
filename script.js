11711aa0dbd55b843563c79df721834a7420e3b6
Deo <57338817+gendeu@users.noreply.github.com>
2025-10-02 22:02:22 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-// Smooth scroll
-document.querySelectorAll('a[href^="#"]').forEach(link => {
-  link.addEventListener("click", function(e) {
+// SPA-like Navigation
+const links = document.querySelectorAll('.nav-link');
+const pages = document.querySelectorAll('.page');
+
+links.forEach(link => {
+  link.addEventListener('click', e => {
     e.preventDefault();
-    document.querySelector(this.getAttribute("href")).scrollIntoView({
-      behavior: "smooth"
-    });
+
+    // remove active
+    links.forEach(l => l.classList.remove('active'));
+    pages.forEach(p => p.classList.remove('active'));
+
+    // set active
+    link.classList.add('active');
+    const target = document.querySelector(link.getAttribute('href'));
+    target.classList.add('active');
   });
 });
